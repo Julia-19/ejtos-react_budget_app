863dceff87aa479e9fdeff224355ceddfd683a33
Julia-19 <121505305+Julia-19@users.noreply.github.com>
2023-07-05 02:00:02 +0530

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,9 +2,20 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    function Count() {
+  const [counter, setCounter] = useState(1);
+  const incrementCounter = () => setCounter(counter + 1);
+  let decrementCounter = () => setCounter(counter - 1);
+  if(counter<=0) {
+    decrementCounter = () => setCounter(1);
+  }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £{budget}<div> 
+      <ButtonIncrement onClickFunc={incrementCounter}/>
+      <Display message={counter}/> 
+      <ButtonDecrement onClickFunc={decrementCounter}/>
+    </div></span>
         </div>
     );
 };
